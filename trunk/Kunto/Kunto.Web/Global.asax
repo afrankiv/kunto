3bf6cc94a43672e11aeb0e4118ddd6b049ebb827
andriyfrankiv@gmail.com <andriyfrankiv@gmail.com@21d4df52-0462-877f-75be-5e73280687ee>
2015-01-14 08:53:25 +0000

    Added index page

--- trunk/Kunto/Kunto.Web/Global.asax
+++ trunk/Kunto/Kunto.Web/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kunto.Web.MvcApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kunto.Web.KuntoMvcApplication" Language="C#" %>
--- trunk/Kunto/Kunto.Web/Global.asax.cs
+++ trunk/Kunto/Kunto.Web/Global.asax.cs
@@ -1,18 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Kunto.Web
 {
-    public class MvcApplication : System.Web.HttpApplication
+    /// <summary>
+    /// </summary>
+    public class KuntoMvcApplication : HttpApplication
     {
+        #region Methods
+
+        /// <summary>
+        /// </summary>
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
+
+        #endregion
     }
 }
