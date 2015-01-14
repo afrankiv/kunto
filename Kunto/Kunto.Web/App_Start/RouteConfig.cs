75ec83d30c20adb52fc9736e9a5689acddbf82cf
andriyfrankiv@gmail.com <andriyfrankiv@gmail.com@21d4df52-0462-877f-75be-5e73280687ee>
2015-01-14 08:53:25 +0000

    Added index page

--- Kunto/Kunto.Web/App_Start/RouteConfig.cs
+++ Kunto/Kunto.Web/App_Start/RouteConfig.cs
@@ -1,23 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Kunto.Web
 {
+    /// <summary>
+    /// Application routings configuration.
+    /// </summary>
     public class RouteConfig
     {
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// </summary>
+        /// <param name="routes">
+        /// </param>
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("Default", "{controller}/{action}/{id}", 
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional });
         }
+
+        #endregion
     }
 }
---
+++ Kunto/Kunto.Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace Kunto.Web.Controllers
+{
+    /// <summary>
+    /// </summary>
+    public class HomeController : Controller
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// </summary>
+        /// <returns>
+        /// </returns>
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        #endregion
+    }
+}
