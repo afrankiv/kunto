ae9c77fc76c69c172ff60d4045181f4bf4abc41e
andriyfrankiv@gmail.com <andriyfrankiv@gmail.com@21d4df52-0462-877f-75be-5e73280687ee>
2015-01-26 07:17:44 +0000

    Clean up code

--- trunk/Kunto/Kunto.Web/App_Start/RouteConfig.cs
+++ trunk/Kunto/Kunto.Web/App_Start/RouteConfig.cs
@@ -2,12 +2,11 @@
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using Kunto.Web.Samples.Infrustructure;
 
 namespace Kunto.Web
 {
     /// <summary>
-    ///     Application routings configuration.
+    /// Application routings configuration.
     /// </summary>
     public class RouteConfig
     {
@@ -16,43 +15,54 @@ namespace Kunto.Web
         /// <summary>
         /// Registers the routes.
         /// </summary>
-        /// <param name="routes">The routes.</param>
+        /// <param name="routes">
+        /// The routes.
+        /// </param>
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             // routes.Add(new Route("handler/{*path}", new CustomRouteHandler { HandlerType = typeof(DayOfWeekHandler) }));
-
-            routes.MapRoute("Default", "{controller}/{action}/{id}",
+            routes.MapRoute("Default", "{controller}/{action}/{id}", 
                 new { controller = "Home", action = "Index", id = UrlParameter.Optional });
         }
 
         #endregion
     }
 
-
     /// <summary>
     /// </summary>
     internal class CustomRouteHandler : IRouteHandler
     {
+        #region Public Properties
+
         /// <summary>
-        ///     Gets or sets the type of the handler.
+        /// Gets or sets the type of the handler.
         /// </summary>
         /// <value>
-        ///     The type of the handler.
+        /// The type of the handler.
         /// </value>
         public Type HandlerType { get; set; }
 
+        #endregion
+
+        #region Public Methods and Operators
+
         /// <summary>
-        ///     Provides the object that processes the request.
+        /// Provides the object that processes the request.
         /// </summary>
-        /// <param name="requestContext">An object that encapsulates information about the request.</param>
+        /// <param name="requestContext">
+        /// An object that encapsulates information about the
+        /// request.
+        /// </param>
         /// <returns>
-        ///     An object that processes the request.
+        /// An object that processes the request.
         /// </returns>
         public IHttpHandler GetHttpHandler(RequestContext requestContext)
         {
-            return (IHttpHandler)Activator.CreateInstance(HandlerType);
+            return (IHttpHandler)Activator.CreateInstance(this.HandlerType);
         }
+
+        #endregion
     }
 }
--- trunk/Kunto/Kunto.Web/Samples/Infrustructure/DayOfWeekHandler.cs
+++ trunk/Kunto/Kunto.Web/Samples/Infrustructure/DayOfWeekHandler.cs
@@ -1,30 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 
 namespace Kunto.Web.Samples.Infrustructure
 {
+    /// <summary>
+    /// </summary>
     public class DayOfWeekHandler : IHttpHandler
     {
+        #region Public Properties
+
+        /// <summary>
+        /// </summary>
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// </summary>
+        /// <param name="context">
+        /// </param>
         public void ProcessRequest(HttpContext context)
         {
             string day = DateTime.Now.DayOfWeek.ToString();
 
-            if (context.Request.CurrentExecutionFilePathExtension == ".json")
-            {
+            if (context.Request.CurrentExecutionFilePathExtension == ".json"){
                 context.Response.ContentType = "application/json";
                 context.Response.Write(string.Format("{{\"day\": \"{0}\"}}", day));
             }
-            else
-            {
+            else{
                 context.Response.ContentType = "text/html";
                 context.Response.Write(string.Format("<span>It is: {0}</span>", day));
             }
         }
-        public bool IsReusable
-        {
-            get { return false; }
-        }
+
+        #endregion
     }
 }
